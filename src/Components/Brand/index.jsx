9cfe484b8884bcf663f59acf0ccbe470264bc04b
rovedabr <rovedabr@gmail.com>
2023-03-05 13:04:12 -0300

    Pasta ícones - Assets de cores inserido

---
+++ src/Components/Brand/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Brand() {
+  return(
+    <Container>
+
+      
+    </Container>
+  )
+}
--- src/Pages/SignIn/styles.js
+++ src/Pages/SignIn/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
 
-  background-color: blue;
+  background-color: ${({theme}) => theme.DARK.DARK_400};
   color: white;
   font-size: 40px;
   text-transform: uppercase;
