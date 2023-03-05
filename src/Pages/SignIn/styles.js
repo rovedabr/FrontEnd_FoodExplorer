d3d4eb5d563c341516368918a5c03db5a16a6813
rovedabr <rovedabr@gmail.com>
2023-03-05 12:25:16 -0300

    Arquivos de temas/ estilos. Criada página SignIn

---
+++ src/Pages/SignIn/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  background-color: blue;
+  color: white;
+  font-size: 40px;
+  text-transform: uppercase;
+`;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { SignIn } from './Pages/SignIn'
+import theme from './Styles/theme'
+import GlobalStyles from "./Styles/global"
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+        <SignIn />
+      <GlobalStyles/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
