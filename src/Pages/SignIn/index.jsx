fdc90dc9390bc4b5d556f4b1a548352492c66a19
rovedabr <rovedabr@gmail.com>
2023-03-07 14:43:48 -0300

    Página de SignIn

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -4,36 +4,32 @@ import { Input } from "../../Components/Input";
 import { Button } from "../../Components/Button";
 import { ButtonText } from "../../Components/ButtonText";
 import { Navbar } from "../../Components/NavBar";
-import { Footer } from "../../Components/Footer";
-import { Header } from "../../Components/Header";
 
 export function SignIn() {
   return (
     <Container>
-      <Header/>
-      {/* <Navbar/> */}
+     <Navbar/>
         <form>        
           <Brand/> 
+          <label>
+            Email    {/* Não está ficando com margem abaixo */}
+            <Input
+              placeholder="Exemplo: exemplo@exemplo.com.br"
+            />
+          </label>
+          <label>
+            Senha
+            <Input
+              placeholder="No mínimo 6 caracteres"
+              />
+          </label>
           <Button>
             Entrar
           </Button>
           <ButtonText
             title="Criar uma conta"
           />
-        <label>
-          Email    {/* Não está ficando com margem abaixo */}
-          <Input
-            placeholder="Exemplo: exemplo@exemplo.com.br"
-          />
-        </label>
-        <label>
-          Senha
-          <Input
-            placeholder="No mínimo 6 caracteres"
-          />
-        </label>
-      </form> 
-      <Footer/>
+        </form> 
     </Container>
   )
 }
--- src/Pages/SignIn/styles.js
+++ src/Pages/SignIn/styles.js
@@ -1,38 +1,55 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100vh;
+width: 100%;
+height: 100vh;
 
+display: grid;
+
+
+body {
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
-  
-  background-color: ${({theme}) => theme.DARK.DARK_400};
-  
-  > div {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
 
-  > form div:first-child {
-    width: 100%;
-    height: 4.4rem;
-  }
+}
 
-  > form {
-    width: 31.6rem;
+background-color: ${({theme}) => theme.DARK.DARK_400};
 
-    border: 1px red solid;
+@media only screen and (min-width: 480px) {
+    width: 480px;
+    height: 100vh;
 
-    label {  
-      font-size: 1.6rem;
-      line-height: 1.6rem;
-      font-weight: 400;
-
-      color: ${({theme}) => theme.LIGHT.LIGHT_400}; 
+    display: grid;
+    grid-template-rows: ;
+    
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     }
+  
+    > form div:first-child {
+      width: 100%;
+      height: 4.4rem;
+    }
+  
+    > form {
+      width: 31.6rem;
+  
+      border: 1px red solid;
+  
+      label {  
+        font-size: 1.6rem;
+        line-height: 1.6rem;
+        font-weight: 400;
+  
+        color: ${({theme}) => theme.LIGHT.LIGHT_400}; 
+      }
+    }
+    
   }
+
 
 `;
