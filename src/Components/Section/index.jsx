bb1376f4023d72be203cd6b338ff9037e800da16
rovedabr <rovedabr@gmail.com>
2023-03-09 19:55:26 -0300

    Página Menu and Component Section - Style

---
+++ src/Components/Section/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+
+export function Section({ title, img, children, ...rest }) {
+  return (
+    <Container>
+
+      
+    </Container>
+
+  )
+
+}
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "../Menu/styles";
+import { Section } from "../../Components/Section";
+
+export function Home() {
+  return (
+    <Container>
+      <Section>
+
+      </Section>
+    </Container>
+  )
+}
