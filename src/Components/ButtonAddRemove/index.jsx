b403927b18650d693280e8d79ccb4a9ccb9febe1
rovedabr <rovedabr@gmail.com>
2023-03-11 16:54:34 -0300

    Component buttonAddRemove  - Style ok

--- src/Components/ButtonAddRemove/index.jsx
+++ src/Components/ButtonAddRemove/index.jsx
@@ -1,9 +1,21 @@
 import { Container } from "./styles";
+import minusIcon from "../../assets/Icons/Minus.svg"
+import plusIcon from "../../assets/Icons/Plus.svg"
 
 export function ButtonAddRemove({ icon: Icon,  ...rest}) {
   return(
     <Container>
-
+      <input 
+        type="image" 
+        id="add" 
+        src={minusIcon}
+      ></input>
+      <span>00</span>
+      <input 
+        type="image" 
+        id="remove"
+        src={plusIcon}
+      />
       
     </Container>
   )
--- src/Components/ButtonAddRemove/styles.js
+++ src/Components/ButtonAddRemove/styles.js
@@ -1,9 +1,34 @@
 import styled from "styled-components";
 
-export const Container = styled.button`
-width: 27rem;
-height: 27rem;
+export const Container = styled.div`
 
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
 
+> #add {
+  width: 2.4rem;
+  height: 2.4rem;
 
+  margin-left: 2.4rem;
+}
+
+> #remove {
+  width: 2.4rem;
+  height: 2.4rem;
+
+  margin-right: 2.4rem;
+}
+
+> span {
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.6rem;
+  line-height: 1.6rem;
+  font-weight: 400;
+
+  color: ${({theme}) => theme.LIGHT.LIGHT_300};
+
+  margin: 0 2.4rem;
+}
 `;
