15ccddbadf5c8f6065e488437b3474a072a395c9
rovedabr <rovedabr@gmail.com>
2023-03-16 12:04:54 -0300

    Página Meal Admin  -  Style Mobile

---
+++ src/Pages/Admin_Meal/index.jsx
@@ -0,0 +1,65 @@
+import { Container, Form } from "./styles";
+
+import { Navbar } from "../../Components/NavBar"
+import { Header } from "../../Components/Header"
+import { Footer } from "../../Components/Footer"
+import { Tag } from "../../Components/Tag"
+import { Button } from "../../Components/Button";
+import { ButtonAddRemove } from "../../Components/ButtonAddRemove";
+
+import receiptIcon from "../../assets/Icons/Receipt.svg"
+import caretLeftIcon from "../../assets/Icons/CaretLeft.svg"
+import saladaRavanello from "../../assets/meals/salada_ravanello.png"
+
+export function AdminMeal() {
+  return(
+    <Container>
+     <Navbar/>
+     <Header/>
+
+      <main>
+        <Form>
+          <button
+            title="button"
+            icon={caretLeftIcon}
+          >
+            <input
+              type="image"
+              src={caretLeftIcon}
+            ></input>
+            Voltar
+          </button>
+
+            <div id="menu">
+                <img 
+                  src={saladaRavanello} 
+                />
+                
+                <div className="content"> 
+
+                  <div className="mealData">
+                    <h2>Salada Ravanello</h2>
+                    <span>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</span>         
+                  </div>   
+
+                  <div className="tags">
+                    <Tag title="alface"></Tag>
+                    <Tag title="cebola"></Tag>
+                    <Tag title="pão naan"></Tag>
+                    <Tag title="pepino"></Tag>
+                    <Tag title="rabanete"></Tag>
+                    <Tag title="tomate"></Tag>
+                  </div>
+
+                  <div className="buttons">
+                    <Button>Editar prato</Button>
+                  </div>
+
+                </div>
+              </div>  
+          </Form>
+        </main> 
+        <Footer/>
+    </Container>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Meal } from './Pages/Meal' 
+import { AdminMeal } from './Pages/Admin_Meal' 
 import theme from './Styles/theme'
 import GlobalStyles from "./Styles/global"
 import { ThemeProvider } from 'styled-components'
@@ -8,7 +8,7 @@ import { ThemeProvider } from 'styled-components'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-        <Meal />
+        <AdminMeal />
       <GlobalStyles/>
     </ThemeProvider>
   </React.StrictMode>,
