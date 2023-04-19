e5a060b58d9e76c7258291d6f470e2ee09c46e68
rovedabr <rovedabr@gmail.com>
2023-04-19 18:23:27 -0300

    Refatoração  Home / Card (corrigido erro soma)

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,8 +2,8 @@ import { Container, Content } from "./styles";
 import { Link } from "react-router-dom";
 import { Button } from "../Button";
 
-import heart from "../../assets/Icons/heart.svg"
 import pencil from "../../assets/Icons/pencil.svg"
+import heart from "../../assets/Icons/heart.svg"
 import minus from "../../assets/Icons/Minus.svg"
 import plus from "../../assets/Icons/Plus.svg"
 
@@ -12,11 +12,12 @@ import { api } from "../../services/api";
 import { useState } from "react";
 
 
-export function Card({ meal, title, icon: Icon, img,image, children, ...rest }) {
+export function Card({ meal, title, children, ...rest }) {
   const { user } = useAuth();
   const isAdmin = user.admin //IsAdmin = 0 (false) | isAdmin = 1 (true)  
 
   const [ quantity, setQuantity ] = useState(Number(0))
+  console.log(quantity)
 
   function handleUpQuantity() {
     setQuantity(prevState => prevState +1)
@@ -37,12 +38,31 @@ export function Card({ meal, title, icon: Icon, img,image, children, ...rest }) 
     >   
         <Content> 
           { isAdmin === 1 ? 
-              <Link to={`editmeal/${meal.id}`}><input type="image" src={pencil} alt="ícone de um lápis" /></Link>
+              <Link 
+                to={`editmeal/${meal.id}`}>
+                  <input 
+                    type="image" 
+                    src={pencil} 
+                    alt="ícone de um lápis" 
+                  />              
+              </Link>
             : 
-              <Link><input type="image" src={heart} alt="ícone de um coração" /></Link>
+              <Link>
+                <input 
+                  type="image" 
+                  src={heart} 
+                  alt="ícone de um coração"                
+                />              
+              </Link>
           }                                
-            <img src={`${api.defaults.baseURL}/files/${meal.image}`} alt="Imagem do prato" />  
-            <Link to={`mealdetails/${meal.id}`}><h2>{meal.title} &gt;</h2></Link>
+            <img 
+              src={`${api.defaults.baseURL}/files/${meal.image}`} 
+              alt="Imagem do prato"             
+            />  
+            <Link 
+              to={`mealdetails/${meal.id}`}>
+                <h2>{meal.title} &gt;</h2>            
+            </Link>
             <p>{meal.description}</p>
             <span>R$ {meal.price}</span>    
           { isAdmin === 1 ? 
@@ -50,11 +70,26 @@ export function Card({ meal, title, icon: Icon, img,image, children, ...rest }) 
             :
               <div className="buttons">
                 <div className="addRemoveButton">
-                  <input type="image" src={minus} alt="Símbolo de menos" id="add" onClick={handleDownQuantity}/>
+                  <input 
+                    type="image" 
+                    src={minus} 
+                    alt="Símbolo de menos" 
+                    id="add" 
+                    onClick={handleDownQuantity}                  
+                  />
                   <span>{quantity}</span>
-                  <input type="image" src={plus} alt="Símbolo de mais" id="remove" onClick={handleUpQuantity}/>
+                  <input 
+                    type="image" src={plus} 
+                    alt="Símbolo de mais" 
+                    id="remove" 
+                    onClick={handleUpQuantity}                  
+                  />
                 </div>
-                <Button title="incluir" />
+                <Button
+                  type="button" 
+                  title="incluir" 
+                
+                />
               </div>          
           }  
 
