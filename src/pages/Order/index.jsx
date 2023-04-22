920fc81bb0f8ce54e5027d576b5f1755d1fb835f
rovedabr <rovedabr@gmail.com>
2023-04-22 13:51:37 -0300

    Criação da página de pedido

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,10 +1,27 @@
-import { Container } from "./styles";
+import { Container, Content, Form } from "./styles";
 
+import { Navbar } from "../../components/NavBar"
+import { Footer } from "../../components/Footer"
 
 export function Order() {
   return (
     <Container>
-
+      <Navbar/>
+        <main>
+          <Content>
+            <h2>Meu pedido</h2>
+              <Form>
+                <div className="order">
+                  <img src="" alt="" />
+                  <p>1 x </p> //! quantity
+                  <h3>Salada</h3> //!title
+                  <span>R$ 25.00</span> //!price
+                </div>
+                <h3>Total: R$ 100,00</h3> //!total order
+              </Form>
+          </Content>
+        </main>
+      <Footer/>  
     </Container>  
   )
 }
--- src/pages/Order/styles.js
+++ src/pages/Order/styles.js
@@ -3,4 +3,47 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
+
+  display: grid;
+  grid-template-areas: 
+   "navbar"
+   "main"
+   "footer";
+  grid-template-columns: 100%;
+  grid-template-rows: 10.4rem auto 7.7rem;
+
+  > nav {
+    grid-area: navbar;    
+  }
+
+  > main{
+    grid-area: main;
+  }
+
+  > footer {
+    grid-area: footer;
+  }
+`;
+
+export const Content = styled.div`
+  width: 30.3rem;
+  height: 65.6rem;
+
+  margin: 5.6rem 9rem 21.4rem 3.5rem;
+
+  > h2 {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 3.2rem;
+    line-height: 4.48rem;
+
+    color: ${({theme}) => theme.LIGHT.LIGHT_300};
+
+    margin-bottom: 2.7rem;
+  }
+`
+export const Form = styled.div`
+  width: 4.5rem;
+  height: 5rem;
+
 `;
